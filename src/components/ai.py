d8bf4886c487e1ebb282e88c74e30d752aed5b13
Maugrift <thenamelessdwarf@gmail.com>
2018-07-15 20:00:57 -0500

    Add a fallback movement strategy for enemies
    This is the same as the move_towards() function that the tutorial implemented in Entity.

--- src/components/ai.py
+++ src/components/ai.py
@@ -7,13 +7,18 @@ class BasicMonster:
     def act(self, game_map, player, fov_map=None):
         results = {}
 
-        distance = self.owner.distance_to(player)
+        dist = self.owner.distance_to(player)
         given_fov_map = fov_map is not None
-        if distance <= self.owner.sight.fov_radius:
-            if distance >= 2:
+        if dist <= self.owner.sight.fov_radius:
+            if dist >= 2:
+                owner_x = self.owner.x
+                owner_y = self.owner.y
+                player_x = player.x
+                player_y = player.y
+
                 if given_fov_map:
-                    libtcod.map_set_properties(fov_map, self.owner.x, self.owner.y, True, True)
-                    libtcod.map_set_properties(fov_map, player.x, player.y, True, True)
+                    libtcod.map_set_properties(fov_map, owner_x, owner_y, True, True)
+                    libtcod.map_set_properties(fov_map, player_x, player_y, True, True)
                     use_fov_map = fov_map
                 else:
                     # This second variable is necessary as creating another variable called fov_map might shadow the
@@ -22,12 +27,21 @@ class BasicMonster:
 
                 # 1.41 approximates sqrt(2), the cost of a diagonal moves
                 path = libtcod.path_new_using_map(use_fov_map, 1.41)
-                libtcod.path_compute(path, self.owner.x, self.owner.y, player.x, player.y)
+                libtcod.path_compute(path, owner_x, owner_y, player_x, player_y)
 
                 if not libtcod.path_is_empty(path) and libtcod.path_size(path) < 25:
                     x, y = libtcod.path_walk(path, True)
                     if x or y:
                         self.owner.move_to(x, y, game_map, face=True)
+                else:
+                    dx = player_x - owner_x
+                    dy = player_y - owner_y
+
+                    dx = int(round(dx / dist))
+                    dy = int(round(dy / dist))
+
+                    if game_map.is_tile_open(owner_x + dx, owner_y + dy):
+                        self.owner.move(dx, dy, game_map)
 
                 libtcod.path_delete(path)
                 if given_fov_map:
